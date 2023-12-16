aa5d231f30811fe16032c1f4015a4c5a11e01e3a
Steven Cervantes <64292955+mrstevencervantes@users.noreply.github.com>
2023-12-16 00:14:07 -0700

    Initial commit

---
+++ SqlServer/setup.sh
@@ -0,0 +1,12 @@
+/*
+
+Enter custom T-SQL here that would run after SQL Server has started up. 
+
+*/
+
+CREATE DATABASE TestDB;
+GO
+USE TestDB;
+CREATE TABLE Inventory (id INT, name NVARCHAR(50), quantity INT);
+INSERT INTO Inventory VALUES (1, 'banana', 150); INSERT INTO Inventory VALUES (2, 'orange', 154);
+GO
